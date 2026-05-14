f490eba64867314afd52adb5f6803d8b622cdb73
Admin <akshaydavet09@gmail.com>
2026-05-14 17:20:22 +0530

    done

--- To-do/src/Item.jsx
+++ To-do/src/Item.jsx
@@ -1,7 +1,8 @@
-function Item({task}){
-    return <ul className="tasks">
-        <li>{task}</li>
-    </ul>
+function Item({deleteFunc, task, id}){
+    return <>
+        <li>{task}&nbsp;&nbsp;&nbsp;<span><button onClick={()=>deleteFunc(id) }>Delete</button></span></li>       
+        </>
+   
 }
 
 export default Item;
--- To-do/src/ToDo.jsx
+++ To-do/src/ToDo.jsx
@@ -1,8 +1,9 @@
 import { useState } from "react"
 import Item from "./Item"
+import { v4 as uuidv4 } from "uuid";
 function ToDo() {
-    let [arr, setArr] = useState(["Eat"]);
-    let [val, setVal] = useState([""]);
+    let [arr, setArr] = useState([{ task: "Sample-Task", id: uuidv4() }]);
+    let [val, setVal] = useState("");
     let UpdateValue = (e) => {
         e.preventDefault();
         setVal(e.target.value);
@@ -10,9 +11,22 @@ function ToDo() {
     let UpdateArr = (e) => {
         e.preventDefault();
         setArr((prevVal) => {
-            return [...prevVal, val]
+            return [...prevVal, { task: val, id: uuidv4() }]
         })
     }
+    let DeleteArr = (id)=>{
+        setArr(arr.filter((item)=> item.id !== id));
+        
+    }
+    let UpdateArr2 = ()=>{
+        setArr(
+             arr.map((item)=>{
+                return item.task.toUpperCase;
+            })
+        
+    }
+    
+    
     // let AddTask = (e)=>{
     //     e.preventDefault();
     //     setArr((prevVal)=>{
@@ -21,11 +35,17 @@ function ToDo() {
     // }  
     return <div className="ToDo">
         <h1>To Do List</h1>
-            <input onChange={UpdateValue} type="text" placeholder="Enter your task here" />
-            <button onClick={UpdateArr} type="submit">Submit</button>
-        {arr.length > 0 && arr.map((item, index) => {
-            return <Item task={item} />
-        })}
+        <input onChange={UpdateValue} type="text" placeholder="Enter your task here" />
+        <button onClick={UpdateArr} type="submit">Submit</button>
+        <ul className="tasks">
+            {arr.length > 0 && arr.map((item, index) => {
+                return <Item deleteFunc={DeleteArr} key={item.id} task={item.task} id={item.id}/>
+            })}
+        </ul>
+            <br />
+            <br />
+            <hr/>
+            <button onClick={UpdateArr2} >Capital tasks</button>
     </div>
 }
 export default ToDo;
